4ddd0353812f7aa37491b75ee3fcbea6e57bb968
31jevi1dpt <31jevi1dpt@hft-stuttgart.de>
2023-12-28 16:17:58 +0100

    initial commit

---
+++ src/main/java/de/hftstuttgart/springboottodovinko/SpringBootTodoVinkoApplication.java
@@ -0,0 +1,13 @@
+package de.hftstuttgart.springboottodovinko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoVinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoVinkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hftstuttgart/springboottodovinko/SpringBootTodoVinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hftstuttgart.springboottodovinko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoVinkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
